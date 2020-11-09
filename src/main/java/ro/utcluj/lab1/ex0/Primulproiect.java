527c9c8448cf4988c393db534997374115423c3b
Misaros Marius <mircea.petrescu@gmail.com>
2020-11-09 18:10:45 +0200

    proiect corect

---
+++ src/main/java/ro/utcluj/lab1/ex0/Primulproiect.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ro.utcluj.lab1.ex0;
+
+/**
+ *
+ * @author Martin
+ */
+public class Primulproiect {
+        public static void main(String[] args){
+        System.out.println("Hello git");
+    }
+}
